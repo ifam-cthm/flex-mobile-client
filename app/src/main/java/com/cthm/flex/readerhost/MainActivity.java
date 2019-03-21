811c5ada5c2b7e0b2a8c2d1f8dde90da5a6b6b9a
jonathas <jonathas.silvassantos@gmail.com>
2019-03-21 09:11:35 -0400

    Alteração na nomeclatura de pacotes

--- app/src/main/java/com/cthm/flex/readerhost/MainActivity.java
+++ app/src/main/java/com/cthm/flex/readerhost/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.flex.readerhost;
+package com.cthm.flex.readerhost;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.cthm.flex.readerhost.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
